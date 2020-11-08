1bb12dc2860663838e0e43dd90765caf08c248df
hentschellj <hentschellj@gmail.com>
2020-11-08 17:55:37 -0600

    Created a moving snake

--- scripts/snake.js
+++ scripts/snake.js
@@ -11,33 +11,39 @@ jQuery(document).ready(function() {
       'y': 0
     },
     {
-      'x': 10,
+      'x': 1,
       'y': 0
     },
     {
-      'x': 20,
+      'x': 2,
       'y': 0
     },
     {
-      'x': 30,
+      'x': 3,
       'y': 0
     },
     {
-      'x': 40,
+      'x': 4,
       'y': 0
     }
   ];
-  
-  createCanvas();
-  createSnake(snake);
+
+  const gameLoop = setInterval(reDraw, 50);
+
+  function reDraw() {
+    console.log('redrawing');
+    createCanvas();
+    createSnake(snake);
+  }
 
   function createCanvas() {
     create(0, 0, width, height, "white", "black");
   }
 
   function createSnake(snakeInput) {
+    updateSnake(snakeInput);
     snakeInput.forEach(function(element) {
-      create(element.x, element.y, snakeSize, snakeSize, "orange", "black");
+      create(element.x*10, element.y*10, snakeSize, snakeSize, "orange", "black");
     });
   }
 
@@ -47,5 +53,12 @@ jQuery(document).ready(function() {
 
     context.strokeStyle = borderColor;
     context.strokeRect(x, y, w, h);
+  }
+
+  function updateSnake(snakeInput) {
+    snakeInput.shift();
+    let cellX = snakeInput[snakeInput.length-1].x+1;
+    let cellY = snakeInput[snakeInput.length-1].y
+    snakeInput.push({ 'x':cellX, 'y':cellY });
   }
 });
