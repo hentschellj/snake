e5eb1b33ede67859a69b8533589a644405720167
hentschellj <hentschellj@gmail.com>
2020-11-08 16:08:40 -0600

    Initial jQuery setup complete

---
+++ index.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+  const snakeCanvas = $('canvas.snake-game')[0];
+  console.log(snakeCanvas);
+  const context     = snakeCanvas.getContext('2d');
+  const width       = snakeCanvas.width;
+  const height      = snakeCanvas.height;
+});
