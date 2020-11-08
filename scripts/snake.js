406362e5a0c22f4005544be59167cb5b9faf7935
hentschellj <hentschellj@gmail.com>
2020-11-08 17:31:48 -0600

    Created game canvas and snake

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,7 +1,51 @@
 jQuery(document).ready(function() {
   const snakeCanvas = $('canvas.snake-game')[0];
-  console.log(snakeCanvas);
   const context     = snakeCanvas.getContext('2d');
   const width       = snakeCanvas.width;
   const height      = snakeCanvas.height;
+  const snakeSize   = 10;
+  
+  const snake = [
+    {
+      'x': 0,
+      'y': 0
+    },
+    {
+      'x': 10,
+      'y': 0
+    },
+    {
+      'x': 20,
+      'y': 0
+    },
+    {
+      'x': 30,
+      'y': 0
+    },
+    {
+      'x': 40,
+      'y': 0
+    }
+  ];
+  
+  createCanvas();
+  createSnake(snake);
+
+  function createCanvas() {
+    create(0, 0, width, height, "white", "black");
+  }
+
+  function createSnake(snakeInput) {
+    snakeInput.forEach(function(element) {
+      create(element.x, element.y, snakeSize, snakeSize, "orange", "black");
+    });
+  }
+
+  function create(x, y, w, h, bgColor, borderColor) {
+    context.fillStyle = bgColor;
+    context.fillRect(x, y, w, h);
+
+    context.strokeStyle = borderColor;
+    context.strokeRect(x, y, w, h);
+  }
 });
