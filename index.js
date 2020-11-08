bb3f1c4c9919e2162e8893caa2defec0a2725cfa
hentschellj <hentschellj@gmail.com>
2020-11-08 17:08:09 -0600

    Uninstalled unnecessary dependencies

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
 
 const PORT = process.env.PORT || 4000
-
-mongoose.connect('mongodb://localhost/snake')
 
 app.use('/scripts', express.static(__dirname + '/scripts'))
 
