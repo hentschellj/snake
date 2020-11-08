0e5764718eb49dfee8612cd5c6566b5067460d08
hentschellj <hentschellj@gmail.com>
2020-11-08 16:12:07 -0600

    Altered js files

--- index.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-  const snakeCanvas = $('canvas.snake-game')[0];
-  console.log(snakeCanvas);
-  const context     = snakeCanvas.getContext('2d');
-  const width       = snakeCanvas.width;
-  const height      = snakeCanvas.height;
-});
